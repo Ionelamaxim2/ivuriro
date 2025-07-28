acaeb136d9be30fd9be614b18e82816a0d5d03bc
Ionela Maxim <ionelamaxim@MacBook-Pro-Ionela.local>
2025-07-28 16:22:43 +0200

    Testimonial-card

--- public/js/main.js
+++ public/js/main.js
@@ -179,3 +179,23 @@ if (successCard || errorCard) {
     if (submitBtn) submitBtn.style.display = "inline-block";
   }, 5000);
 }
+
+const cards = document.querySelectorAll(".tilt-card");
+
+cards.forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = -(y - centerY) / 15;
+    const rotateY = (x - centerX) / 15;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  });
+});
